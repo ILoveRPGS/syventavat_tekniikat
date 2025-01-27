5b40653734c56f3a4d9a2514250b56cf1216a6a0
ILoveRPGS <mitja.savijarvi@gmail.com>
2025-01-27 10:28:36 +0200

    Muutokset

--- TehtavaViikko2/src/Birthday.java
+++ TehtavaViikko2/src/Birthday.java
@@ -4,30 +4,26 @@ import java.time.temporal.ChronoUnit;
 
 public class Birthday {
     public static void main(String[] args) throws Exception {
-        while(true){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Anna syntymäpäiväsi (vvvv.kk.pp): ");
-            String syntymapaiva = scanner.nextLine();
-            String[] syntymapaivaArray = syntymapaiva.split("\\.");
-            int syntymapaivaPaiva = Integer.parseInt(syntymapaivaArray[2]);
-            int syntymapaivaKuukausi = Integer.parseInt(syntymapaivaArray[1]);
-            int syntymapaivaVuosi = Integer.parseInt(syntymapaivaArray[0]);
-            LocalDate syntymapaivaDate = LocalDate.of(syntymapaivaVuosi, syntymapaivaKuukausi, syntymapaivaPaiva);
+            String syntymapaiva = System.getenv("BIRTHDAY");
+            try{
+            LocalDate syntymapaivaDate = LocalDate.parse(syntymapaiva);
             LocalDate tanaan = LocalDate.now();
-
             long erotus = ChronoUnit.DAYS.between(syntymapaivaDate, tanaan);
-        if(syntymapaivaDate.isAfter(tanaan)){
-            System.out.println("Syntymäpäivä ei voi olla tulevaisuudessa.");
-        } 
-        if(syntymapaivaDate.getMonthValue() == tanaan.getMonthValue() && syntymapaivaDate.getDayOfMonth() == tanaan.getDayOfMonth()){
-            System.out.println("Hyvää syntymäpäivää!");
-        }
-        if(erotus % 1000 == 0){
-            System.out.println("Eletyt päiväsi ovat tasan jaollisia tuhannella!"+ erotus);
-        }
-        if(syntymapaivaDate.isBefore(tanaan)){ 
-            System.out.println("Olet elänyt " + erotus + " päivää.");
-        }
-    }
+            if(syntymapaivaDate.isAfter(tanaan)){
+                System.out.println("Syntymäpäivä ei voi olla tulevaisuudessa.");
+            } 
+            if(syntymapaivaDate.getMonthValue() == tanaan.getMonthValue() && syntymapaivaDate.getDayOfMonth() == tanaan.getDayOfMonth()){
+                System.out.println("Hyvää syntymäpäivää!");
+            }
+            if(erotus % 1000 == 0){
+                System.out.println("Eletyt päiväsi ovat tasan jaollisia tuhannella! "+ erotus);
+            }
+            if(syntymapaivaDate.isBefore(tanaan)){ 
+                System.out.println("Olet elänyt " + erotus + " päivää.");
+            }
+            } catch (Exception e){
+                System.out.println("Anna syntymäpäivä muodossa vvvv-kk-pp terminaaliin.");
+            }
+
 }
 }
